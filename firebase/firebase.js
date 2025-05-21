b4ac636883cf6985dd4ab54953ba8b6368d364ff
Parth Savani <104356005+SParth033@users.noreply.github.com>
2025-05-21 21:47:22 +0530

    Remove service account key reference in Firebase initialization for improved security; update admin credential setup to use default configuration.

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,9 +1,8 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json"); // Path to your downloaded JSON
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(),
 });
 
 module.exports = admin;
