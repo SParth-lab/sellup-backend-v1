2a3da5002c22329d82dfc851180a33c97e1aa158
Parth Savani <85479058+SParth-lab@users.noreply.github.com>
2025-09-18 15:45:42 +0530

    Add verifyToken in send-otp in via Whatsapp

--- Routes/OTPRoute.js
+++ Routes/OTPRoute.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const { viaWhatsapp, verifyOTP } = require('../Controllers/OTPController.js');
+const verifyToken  = require('../Helper/VerifyToken.js');
 const router = Router();
 
 
-router.post("/send-otp", viaWhatsapp.validator, viaWhatsapp.controller);
+router.post("/send-otp", verifyToken, viaWhatsapp.validator, viaWhatsapp.controller);
 
 router.post("/verify-otp", verifyOTP.validator, verifyOTP.controller);
 
